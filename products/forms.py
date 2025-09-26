a3049206eb0026564b78ad45ff922dcf5e04094e
Robson José Lopes G. Júnior <robsonlopes.jr@gmail.com>
2025-09-26 18:40:39 -0300

    adicionado o crud de produtos

---
+++ products/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from . import models
+
+
+class ProductflowForm(forms.ModelForm):
+    class Meta:
+        model = models.Product
+        fields = ['title', 'category', 'brand', 'serie_number', 'cost_price', 'selling_price', 'description']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'brand': forms.Select(attrs={'class': 'form-control'}),
+            'serie_number': forms.TextInput(attrs={'class': 'form-control'}),
+            'cost_price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'selling_price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
+        labels = {
+            'title': 'Produto',
+            'category': 'Categoria',
+            'brand': 'Marca',
+            'serie_number': 'Número de Série',
+            'cost_price': 'Preço de Custo',
+            'selling_price': 'Preço de Venda',
+            'description': 'Descrição',
+        }
