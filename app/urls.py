83cb91dceb54aa7db023399be1f26badf128803e
Robson José Lopes G. Júnior <robsonlopes.jr@gmail.com>
2025-09-26 18:19:51 -0300

    adicionado o crud de saidas

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', include('categories.urls')),
     path('', include('suppliers.urls')),
     path('', include('inflows.urls')),
+    path('', include('outflows.urls')),
 ]
