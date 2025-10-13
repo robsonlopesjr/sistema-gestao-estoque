a4b8244b65e6a0ceaff20e9a07aac9d7bb3e2dba
Robson José Lopes G. Júnior <robsonlopes.jr@gmail.com>
2025-10-13 19:43:56 -0300

    criando métricas de vendas

--- app/views.py
+++ app/views.py
@@ -4,8 +4,10 @@ from . import metrics
 def home(request):
     
     product_metrics = metrics.get_product_metrics()
+    sales_metrics = metrics.get_sales_metrics()
 
     context = {
-        'product_metrics': product_metrics
+        'product_metrics': product_metrics,
+        'sales_metrics': sales_metrics
     }
     return render(request, 'home.html', context)
