63c8b048663d7dada2b03eb357636cd9a3c99549
Robson José Lopes G. Júnior <robsonlopes.jr@gmail.com>
2025-10-20 17:18:02 -0300

    criando tela e rota de login

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login', auth_views.LoginView.as_view(), name='login'),
     path('', views.home, name='home'),
     path('', include('brands.urls')),
     path('', include('categories.urls')),
