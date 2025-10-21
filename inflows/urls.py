f9824114087e69bb143a9a749911742852c2746d
Robson José Lopes G. Júnior <robsonlopes.jr@gmail.com>
2025-10-21 16:42:19 -0300

    adicionando rota para api de entradas

--- inflows/urls.py
+++ inflows/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('inflows/list/', views.InflowListView.as_view(), name='inflow_list'),
     path('inflows/create/', views.InflowCreateView.as_view(), name='inflow_create'),
     path('inflows/<int:pk>/detail/', views.InflowDetailView.as_view(), name='inflow_detail'),
+
+    path('api/v1/inflows/', views.InflowListCreateAPIView.as_view(), name='inflow-list-create-api-view'),
+    path('api/v1/inflows/<int:pk>', views.InflowRetrieveAPIView.as_view(), name='inflow-detail-api-view'),
 ]
