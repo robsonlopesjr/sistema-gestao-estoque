96ce7dc98dd9d06fa769feb195fa39e07e2d0f1e
Robson José Lopes G. Júnior <robsonlopes.jr@gmail.com>
2025-10-21 11:49:21 -0300

    implementando logout

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.home, name='home'),
     path('', include('brands.urls')),
     path('', include('categories.urls')),
