aad08b83221e16773975a43935fc8c9ad1e60891
Robson José Lopes G. Júnior <robsonlopes.jr@gmail.com>
2025-10-21 16:27:34 -0300

    adicionando rota para api de categorias

--- categories/urls.py
+++ categories/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('categories/<int:pk>/detail/', views.CategoryDetailView.as_view(), name='category_detail'),
     path('categories/<int:pk>/update/', views.CategoryUpdateView.as_view(), name='category_update'),
     path('categories/<int:pk>/delete/', views.CategoryDeleteView.as_view(), name='category_delete'),
+
+    path('api/v1/categories/', views.CategoryListCreateAPIView.as_view(), name='category-list-create-api-view'),
+    path('api/v1/categories/<int:pk>', views.CategoryRetrieveUpdateDestroyAPIView.as_view(), name='category-detail-update-api-view'),
 ]
